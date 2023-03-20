a6aba8dfc72b01386cea98cf704e108740649255
levideutsch <levideutsch@live.com>
2023-03-20 16:54:46 -0400

    commit tree 523ad3258207caf482b58c74ed9b6155b427c9a4

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive)VALUES("Mr. Chocolate", 1, "M", "White", "Calm", true);
+INSERT INTO bears (name, age, sex, color, temperament, alive)VALUES("Rowdy", 2, "F", "Black", "Angry", false);
+INSERT INTO bears (name, age, sex, color, temperament, alive)VALUES("Tabitha", 3, "M", "White", "Calm", true);
+INSERT INTO bears (name, age, sex, color, temperament, alive)VALUES("Sergeant Brown", 4, "F", "Black", "Angry", false);
+INSERT INTO bears (name, age, sex, color, temperament, alive)VALUES("Melissa", 5, "M", "White", "Calm", true);
+INSERT INTO bears (name, age, sex, color, temperament, alive)VALUES("Grinch", 6, "F", "Black", "Angry", false);
+INSERT INTO bears (name, age, sex, color, temperament, alive)VALUES("Wendy", 7, "M", "White", "Calm", true);
+INSERT INTO bears (name, age, sex, color, temperament, alive)VALUES(NULL, 8, "F", "Black", "Angry", false);
+
